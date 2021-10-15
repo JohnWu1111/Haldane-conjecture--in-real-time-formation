4b7655008752395d511826cb0bea222e143bf6fc
JohnWu1111 <johnwu@sjtu.edu.cn>
2021-10-15 10:14:45 +0800

    Fix bugs
    Fix bugs, now the programs are right

--- spin_half.m
+++ spin_half.m
@@ -8,9 +8,11 @@ sigma_z = [ 1 0; 0 -1];
 sigma_x = [ 0 1; 1 0];
 I = eye(2);
 H = zeros(len,len);
-h = h0.*[1 0.8 0.6 0.4 -0.4 -0.6 -0.8 -1];
-dt = 0.02;
-t = 0:dt:10;
+% h = h0.*[1 0.8 0.6 0.4 -0.4 -0.6 -0.8 -1];
+h = h0.*[1 0.75 0.5 0.25 -0.25 -0.5 -0.75 -1];
+% h = h0.*[1 1 1 1 -1 -1 -1 -1];
+dt = 0.1;
+t = 0:dt:100;
 
 % pos=1单独赋值
 H1 = sigma_z;
@@ -33,20 +35,19 @@ for i = 2:L-1
     H = H - H1;
 end
 
-% pos=L单独赋值
-H1 = sigma_z;
-for j = 2:L-1
-    H1 = kron(H1,I);
-end
-H1 = kron(H1,sigma_z);
-H = H - H1;
+% % pos=L单独赋值
+% H1 = sigma_z;
+% for j = 2:L-1
+%     H1 = kron(H1,I);
+% end
+% H1 = kron(H1,sigma_z);
+% H = H - H1;
 
 % pos=1单独赋值
 H2 = sigma_x;
 for j = 2:L
     H2 = kron(H2,I);
 end
-sigma_x1 = H2;
 H = H + h(i).*H2;
 
 for i = 2:L
@@ -78,14 +79,23 @@ tran = diag(temp);
 for i = 2:length(t)
     a = tran*a;
     G(i) = b'*a;
-%     G(i) = G(i)^2;
+%     G(i) = -log(abs(G(i)))/L;
+    G(i) = real(G(i));
 end
 
+% for i = 2:length(t)
+%     temp = exp(-1i*e*t(i));
+%     tran = diag(temp);
+%     phi = tran*a;
+%     G(i) = b'*phi;
+% %     G(i) = G(i)^2;
+% end
+
 figure;
-plot(t,real(G));
+plot(t,G);
 xlabel('time')
 ylabel('transition probility')
-str = strcat('transition probility of spin haf');
+str = strcat('transition probility of spin half');
 title(str)
 fname = ['transition probility_spin half','.png '];
 saveas(gcf, fname, 'png')
--- spin_int.m
+++ spin_int.m
@@ -5,12 +5,14 @@ L = 8;
 len = 3^L;
 h0 = 1;
 sigma_z = [ 1 0 0; 0 0 0; 0 0 -1];
-sigma_x = [ 0 1 0; 1 0 1; 0 1 0]./sqrt(2);
+sigma_x = [ 0 1 0; 1 0 1; 0 1 0].*sqrt(2);
 I = eye(3);
 H = zeros(len,len);
-h = h0.*[1 0.8 0.6 0.4 -0.4 -0.6 -0.8 -1];
-dt = 0.02;
-t = 0:dt:10;
+% h = h0.*[1 0.8 0.6 0.4 -0.4 -0.6 -0.8 -1];
+h = h0.*[1 0.75 0.5 0.25 -0.25 -0.5 -0.75 -1];
+% h = h0.*[1 1 1 1 -1 -1 -1 -1];
+dt = 1;
+t = 0:dt:1000;
 
 % pos=1单独赋值
 H1 = sigma_z;
@@ -33,20 +35,19 @@ for i = 2:L-1
     H = H - H1;
 end
 
-% pos=L单独赋值
-H1 = sigma_z;
-for j = 2:L-1
-    H1 = kron(H1,I);
-end
-H1 = kron(H1,sigma_z);
-H = H - H1;
+% % pos=L单独赋值
+% H1 = sigma_z;
+% for j = 2:L-1
+%     H1 = kron(H1,I);
+% end
+% H1 = kron(H1,sigma_z);
+% H = H - H1;
 
 % pos=1单独赋值
 H2 = sigma_x;
 for j = 2:L
     H2 = kron(H2,I);
 end
-sigma_x1 = H2;
 H = H + h(i).*H2;
 
 for i = 2:L
@@ -75,22 +76,31 @@ phi1(end) = 1;
 G = zeros(length(t),1);
 
 G(1) = phi1'*phi0;
-a = V*phi0;
-b = V*phi1;
+a = V'*phi0;
+b = V'*phi1;
 e = diag(D);
 temp = exp(-1i*e*dt);
 tran = diag(temp);
 for i = 2:length(t)
     a = tran*a;
-    G(i) = real(b'*a);
-%     G(i) = G(i)^2;
+    G(i) = b'*a;
+%     G(i) = -log(abs(G(i)))/L;
+    G(i) = real(G(i));
 end
+
+% for i = 2:length(t)
+%     temp = exp(-1i*e*t(i));
+%     tran = diag(temp);
+%     phi = tran*a;
+%     G(i) = b'*phi;
+% %     G(i) = G(i)^2;
+% end
 
 figure;
 plot(t,G);
 xlabel('time')
 ylabel('transition probility')
-str = strcat('transition probilityof spin int');
+str = strcat('transition probility of spin int');
 title(str)
 fname = ['transition probility_spin int','.png '];
 saveas(gcf, fname, 'png')
